f72f8bdaf7aa62d9bbfed0e4ddea8bdfbc87ddf7
Claire Hunsaker <claire@stormpath.com>
2016-06-16 09:37:44 -0700

    Intial repo

---
+++ src/main/java/com/stormpath/jjwtsite/JjwtftwApplication.java
@@ -0,0 +1,12 @@
+package com.stormpath.jjwtsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjwtftwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjwtftwApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stormpath/jjwtsite/JjwtftwApplicationTests.java
@@ -0,0 +1,18 @@
+package com.stormpath.jjwtsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JjwtftwApplication.class)
+@WebAppConfiguration
+public class JjwtftwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
